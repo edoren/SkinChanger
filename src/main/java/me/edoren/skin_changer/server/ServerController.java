1506dbcda77851728b4b306b89c1bc7e56212258
Manuel Sabogal <mfer32@gmail.com>
2024-07-10 09:38:58 +0200

    Move command registration to the right place
    (cherry picked from commit 699cade8a0259c1073be46b925212e95da6e3341)

--- src/main/java/me/edoren/skin_changer/server/ServerController.java
+++ src/main/java/me/edoren/skin_changer/server/ServerController.java
@@ -21,8 +21,6 @@ public class ServerController {
     }
 
     public void setServer(MinecraftServer server) {
-        SkinsCommand.register(server.getFunctions().getDispatcher());
-
         Path savesFolder = server.getWorldPath(LevelResource.ROOT);
         File skinChangerFolder = Paths.get(savesFolder.toString(), "skin_changer").normalize().toFile();
 
