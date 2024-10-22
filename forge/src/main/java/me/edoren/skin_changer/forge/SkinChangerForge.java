e9a4078f6c0bb7475aa442a248ea822fab8d4509
Manuel Sabogal <mfer32@gmail.com>
2024-10-23 00:56:13 +0200

    Add support for Fabric through architectury

--- forge/src/main/java/me/edoren/skin_changer/forge/SkinChangerForge.java
+++ forge/src/main/java/me/edoren/skin_changer/forge/SkinChangerForge.java
@@ -1,8 +1,9 @@
 package me.edoren.skin_changer.forge;
 
 import me.edoren.skin_changer.SkinChanger;
-import me.edoren.skin_changer.common.Constants;
+import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class SkinChangerForge {
@@ -10,6 +11,9 @@ public class SkinChangerForge {
 
     @SuppressWarnings("unused")
     public SkinChangerForge() {
+        // Submit our event bus to let Architectury API register our content on the right time.
+        EventBuses.registerModEventBus(SkinChanger.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
         this.instance = new SkinChanger();
     }
 }
