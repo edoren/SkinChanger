ec1779bc91ea70990818492bbf446bfc996fa6fa
Manuel Sabogal <mfer32@gmail.com>
2024-10-26 03:00:18 +0200

    Move command registration to the right place
    - Related to: 699cade8a0259c1073be46b925212e95da6e3341
    
    (cherry picked from commit 18d696447653e85ff9ef8bfca82680f5336126b1)

--- common/src/main/java/me/edoren/skin_changer/SkinChanger.java
+++ common/src/main/java/me/edoren/skin_changer/SkinChanger.java
@@ -1,15 +1,19 @@
 package me.edoren.skin_changer;
 
+import com.mojang.brigadier.CommandDispatcher;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
+import dev.architectury.event.events.common.CommandRegistrationEvent;
 import dev.architectury.event.events.common.LifecycleEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import me.edoren.skin_changer.client.ClientController;
 import me.edoren.skin_changer.common.NetworkContext;
 import me.edoren.skin_changer.server.ServerController;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import me.edoren.skin_changer.server.SkinsCommand;
 import net.minecraft.client.Minecraft;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import net.minecraft.server.MinecraftServer;
 
 public final class SkinChanger {
@@ -19,6 +23,7 @@ public final class SkinChanger {
             ClientLifecycleEvent.CLIENT_SETUP.register(this::onClientSetup);
         }
         LifecycleEvent.SERVER_STARTED.register(this::onServerStarted);
+        CommandRegistrationEvent.EVENT.register(this::onCommandRegistration);
     }
 
     void onSetup() {
@@ -31,5 +36,9 @@ public final class SkinChanger {
 
     void onServerStarted(MinecraftServer server) {
         ServerController.GetInstance().initialize(server);
+    }
+
+    void onCommandRegistration(CommandDispatcher<CommandSourceStack> commandSourceStackCommandDispatcher, CommandBuildContext commandBuildContext, Commands.CommandSelection commandSelection) {
+        SkinsCommand.register(commandSourceStackCommandDispatcher);
     }
 }
