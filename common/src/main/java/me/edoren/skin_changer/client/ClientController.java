88a1eda7aa4082f7b853c891af659c306ddd1979
Manuel Sabogal <mfer32@gmail.com>
2024-10-26 20:54:01 +0200

    Make sure to unregister only the specific listeners

--- common/src/main/java/me/edoren/skin_changer/client/ClientController.java
+++ common/src/main/java/me/edoren/skin_changer/client/ClientController.java
@@ -31,8 +31,11 @@ public class ClientController {
     }
 
     public void initialize() {
-        ClientTickEvent.CLIENT_PRE.clearListeners();
         ClientTickEvent.CLIENT_PRE.register(this::onClientTickEvent);
+    }
+
+    public void deinitialize() {
+        ClientTickEvent.CLIENT_PRE.unregister(this::onClientTickEvent);
     }
 
     public ResourceLocation getLocationCape(PlayerModel model) {
